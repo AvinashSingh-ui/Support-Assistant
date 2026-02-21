3d4e71e080919d7b7b52ee5bf7ca8ee981bc628a
Avinashsingh-ui <avinashsingh60005@gmail.com>
2026-02-21 23:05:02 +0530

    package.json

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,31 +1,22 @@
-import { scalekit } from "../../../../../lib/scalekit";
+import { scalekit } from "@/lib/scalekit";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-    const { searchParams } = new URL(req.url);
-    const code = searchParams.get("code");
-
-    const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
-    if (!baseUrl) {
-        throw new Error("NEXT_PUBLIC_APP_URL is not defined");
+export async function GET(req:NextRequest) {
+    const {searchParams}=new URL(req.url)
+    const code=searchParams.get("code")
+     const redirectUri=`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`
+    if(!code){
+        return NextResponse.json({message:"code is not found"},{status:400})
     }
+    const session=await scalekit.authenticateWithCode(code,redirectUri)
+    console.log(session)
+  const response= NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}`)
+  response.cookies.set("access_token",session.accessToken,{
+    httpOnly:true,
+    maxAge:24*60*60*1000,
+    secure:true,
+    path:"/"
+  })
 
-    const redirectUri = `${baseUrl}/api/auth/callback`;
-
-    if (!code) {
-        return NextResponse.json({ error: "Code not found" }, { status: 400 });
-    }
-
-    const session = await scalekit.authenticateWithCode(code, redirectUri);
-
-    const response = NextResponse.redirect(baseUrl);
-
-    response.cookies.set("access_token", session.accessToken, {
-        httpOnly: true,
-        secure: false,
-        maxAge: 60 * 60 * 24,
-        path: "/",
-    });
-
-    return response;
+  return response
 }
